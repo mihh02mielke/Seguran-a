66d14ca260891e648443494d06e92d6e0f5344af
mihh02mielke <Millena.mielke@escola.pr.gov.br>
2026-08-13 13:40:10 +0000

    aula 8 +

--- main.js
+++ main.js
@@ -80,5 +80,5 @@ function geraSenha(){
     }
 }
 const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
